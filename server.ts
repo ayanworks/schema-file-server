116e4d4fc0f9ab3daf09c8c9be0f1a4c4c09da4a
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2023-12-26 17:30:01 +0530

    feat: add authentication
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- server.ts
+++ server.ts
@@ -1,10 +1,36 @@
-import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts"
-
-// routes
-import schemaRouter from "./routes/schema.ts"
+import { Application, Middleware } from "https://deno.land/x/oak@v12.6.1/mod.ts"
+import {
+  IgnorePattern,
+  jwtMiddleware,
+} from "https://deno.land/x/oak_middleware_jwt@2.2.2/mod.ts"
 import { APP_PORT } from "./config.ts"
+import { secretKey } from "./utils/utils.ts"
+import schemaRouter from "./routes/schema.ts"
 
 const app = new Application()
+
+const ignorePatterns: IgnorePattern[] = [
+  {
+    path: "/",
+    methods: ["GET"],
+  },
+  {
+    path: /[a-zA-Z0-9]+/,
+    methods: ["GET"],
+  },
+  {
+    path: /^\/schemas\/[a-zA-Z0-9]+/,
+    methods: ["GET"],
+  },
+]
+
+app.use(
+  jwtMiddleware<Middleware>({
+    key: secretKey,
+    algorithm: "HS256",
+    ignorePatterns,
+  })
+)
 
 app.use(schemaRouter.routes())
 app.use(schemaRouter.allowedMethods())
@@ -18,3 +44,7 @@ app.use(({ response }) => {
 app.listen({ port: Number(APP_PORT) })
 
 console.log(`Listening on port: ${APP_PORT}...`)
+
+// Get auth token
+// const { token, payload } = await getAuthToken()
+// console.log("getAuthToken", token, payload)
---
+++ utils/utils.ts
@@ -0,0 +1,30 @@
+import { Header, Payload, create } from "https://deno.land/x/djwt@v3.0.1/mod.ts"
+import * as base64 from "https://deno.land/std@0.207.0/encoding/base64.ts"
+import { JWT_TOKEN_SECRET } from "../config.ts"
+
+export const secretKey = await crypto.subtle.importKey(
+  "raw",
+  base64.decodeBase64(JWT_TOKEN_SECRET),
+  { name: "HMAC", hash: "SHA-256" },
+  true,
+  ["sign", "verify"]
+)
+
+const header: Header = {
+  alg: "HS256",
+  typ: "JWT",
+}
+
+export const getAuthToken = async () => {
+  const payload: Payload = {
+    iss: "AyanWorks",
+    id: crypto.randomUUID(),
+  }
+
+  const jwt = await create(header, payload, secretKey)
+
+  return {
+    token: jwt,
+    payload,
+  }
+}
